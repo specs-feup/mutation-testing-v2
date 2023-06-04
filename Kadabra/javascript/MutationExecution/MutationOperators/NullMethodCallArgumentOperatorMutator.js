60460dbbdeda6ea3e4c8483ff3687ed821b79d7a
RitaVeiga <up202103388@up.pt>
2023-06-04 20:15:05 +0100

    add operator

--- Kadabra/javascript/MutationExecution/MutationOperators/NullMethodCallArgumentOperatorMutator.js
+++ Kadabra/javascript/MutationExecution/MutationOperators/NullMethodCallArgumentOperatorMutator.js
@@ -3,39 +3,41 @@ laraImport("kadabra.KadabraNodes");
 laraImport("weaver.WeaverJps");
 laraImport("weaver.Weaver");
 
-class NotSerializableOperatorMutator extends Mutator {
+class NullMethodCallArgumentOperatorMutator extends Mutator {
     constructor() {
-        super("NotSerializableOperatorMutator");
+        super("NullMethodCallArgumentOperatorMutator");
 
         this.mutationPoints = [];
         this.currentIndex = 0;
         this.mutationPoint = undefined;
         this.previousValue = undefined;
-        this.removedInterface = undefined;
+        this.dataTypeOfSecondParam = undefined;
     }
-    
-    isAndroidSpecific(){
-      return false;
+
+    isAndroidSpecific() {
+        return false;
     }
 
     addJp(joinpoint) {
-      
-        if (joinpoint.instanceOf("class")) {
-            if (joinpoint.interfaces.contains("java.io.Serializable")) {
-                this.mutationPoints.push(joinpoint);
+
+        if (joinpoint.instanceOf('callStatement')) {
+
+            if (joinpoint.call.children[1].instanceOf('var')) {
+                this.mutationPoints.push(joinpoint.call.children[1]);
+
                 return true;
             }
         }
+
         return false;
     }
+
     hasMutations() {
         return this.currentIndex < this.mutationPoints.length;
     }
 
     getMutationPoint() {
-
         if (this.isMutated) {
-            println("this.isMutated   " + this.mutationPoint);
             return this.mutationPoint;
         } else {
             if (this.currentIndex < this.mutationPoints.length) {
@@ -47,45 +49,41 @@ class NotSerializableOperatorMutator extends Mutator {
     }
 
     _mutatePrivate() {
+
         this.mutationPoint = this.mutationPoints[this.currentIndex];
 
         this.currentIndex++;
 
+        this.previousValue = this.mutationPoint.copy();
 
-        this.previousValue = this.mutationPoint;
-        println("this.previousValue   " + this.previousValue);
 
-        this.removedInterface = this.mutationPoint.removeInterface("java.io.Serializable");
-
-        println("this.removedInterface   " + this.removedInterface);
+        this.mutationPoint = this.mutationPoint.insertReplace(null);
 
 
         println("/*--------------------------------------*/");
         println("Mutating operator n." + this.currentIndex + ": " + this.previousValue
-            + " to " + "\"\"");
+            + " to " + this.mutationPoint);
         println("/*--------------------------------------*/");
 
 
-        println(" this.mutationPoint" + this.mutationPoint);
     }
 
     _restorePrivate() {
-        this.mutationPoint = this.mutationPoint.addInterface(this.removedInterface);
+        this.mutationPoint = this.mutationPoint.insertReplace(this.previousValue);
         this.previousValue = undefined;
         this.mutationPoint = undefined;
-
     }
 
 
     toString() {
-        return `Not Serializable Operator Mutator from ${this.previousValue} to ${this.mutationPoint}, current mutation points ${this.mutationPoints}, current mutation point ${this.mutationPoint} and previous value ${this.previousValue}`;
+        return `Null Method Call Argument Operator Mutator from ${this.previousValue} to ${this.mutationPoint}, current mutation points ${this.mutationPoints}, current mutation point ${this.mutationPoint} and previous value ${this.previousValue}`;
     }
+
     toJson() {
         return {
             mutationOperatorArgumentsList: [],
             operator: this.name,
             isAndroidSpecific: this.isAndroidSpecific(),
-        }
+        };
     }
-
 }
