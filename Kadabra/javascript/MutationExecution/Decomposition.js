bf2884bc23808366de8897cabfd02fe620598007
João Bispo <jbispo@fe.up.pt>
2023-10-21 18:49:33 +0100

    Fixes test in Decomposition

--- Kadabra/javascript/MutationExecution/Decomposition.js
+++ Kadabra/javascript/MutationExecution/Decomposition.js
@@ -1,18 +1,25 @@
+laraImport("kadabra.KadabraNodes");
+
 function changeVarDeclarations() {
   for (var jp of Query.root().descendants) {
     if (
       jp.instanceOf("localVariable") &&
-      jp.numChildren >= 2 &&
+      jp.init !== undefined &&
       !(jp.parent.type == "for") &&
       !jp.parent.instanceOf("try")
     ) {
+
+      
       // Create write reference to local variable
       let localVar = KadabraNodes.var(jp, true);
+      
       // Get initialization, and remove it from declaration
       const varLhs = jp.init;
       jp.init = undefined;
+      
       // Create assignment
       const varAssign = KadabraNodes.assignment(localVar, varLhs);
+      
       // Add assignment after initialization
       jp.insertAfter(varAssign);
     }
