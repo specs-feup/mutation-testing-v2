bb59b42689f372fb946723a8d23634f556e68568
João Bispo <jbispo@fe.up.pt>
2023-11-27 15:40:22 +0000

    Adds ; to new and call statements

--- Kadabra/javascript/MutationExecution/MutatorUtils.js
+++ Kadabra/javascript/MutationExecution/MutatorUtils.js
@@ -1,103 +1,106 @@
 class MutatorUtils {
-
-    /**
-     * 
-     * @param {*} stmt 
-     * @returns true if given statement needs a semi-colon, false otherwise
-     */
-    static needsSemiColon(stmt) {
-
-        if (stmt.instanceOf("if") || stmt.instanceOf("loop") || stmt.instanceOf("try") || stmt.instanceOf("switch")) {
-            //println("NOT ADDING ; ->  " + stmt.joinPointType);
-            return false;
-        }
-
-        //println("ADDING ; ->  " + stmt.joinPointType);
-        return true;
+  /**
+   *
+   * @param {*} stmt
+   * @returns true if given statement needs a semi-colon, false otherwise
+   */
+  static needsSemiColon(stmt) {
+    if (
+      stmt.instanceOf("if") ||
+      stmt.instanceOf("loop") ||
+      stmt.instanceOf("try") ||
+      stmt.instanceOf("switch") ||
+      stmt.instanceOf("new") ||
+      stmt.instanceOf("call") ||
+      stmt.instanceOf("callStatement")
+    ) {
+      //println("NOT ADDING ; ->  " + stmt.joinPointType);
+      return false;
     }
 
-    /**
-     * 
-     * @param {*} $stmt 
-     * @returns {Boolean} true if the given node returns in some way (return instruction, throw instruction, etc), false if execution continues from that point on
-     */
-    static isReturningStmt($stmt) {
+    //println("ADDING ; ->  " + stmt.joinPointType);
+    return true;
+  }
 
-        if ($stmt.instanceOf("return") || $stmt.instanceOf("throw")) {
-            return true;
+  /**
+   *
+   * @param {*} $stmt
+   * @returns {Boolean} true if the given node returns in some way (return instruction, throw instruction, etc), false if execution continues from that point on
+   */
+  static isReturningStmt($stmt) {
+    if ($stmt.instanceOf("return") || $stmt.instanceOf("throw")) {
+      return true;
+    }
+
+    // If 'try', body and catches must all return true for it to be a returning stmt
+    if ($stmt.instanceOf("try")) {
+      let result = MutatorUtils.isReturningStmt($stmt.body);
+
+      for (var currentCatch of $stmt.catches) {
+        //println("CATCH AST:\n" + currentCatch.ast);
+        //println("CATCH BODY AST:\n" + currentCatch.body.ast);
+        result = result & MutatorUtils.isReturningStmt(currentCatch.body);
+      }
+
+      return result;
+    }
+
+    // If 'switch', true if all cases return
+    if ($stmt.instanceOf("switch")) {
+      let result = true;
+      for (const switchCase of $stmt.cases) {
+        const stmts = switchCase.stmts;
+
+        // If empty, assume fall-through to next case,
+        // leaving result unchanged
+        if (stmts.length === 0) {
+          continue;
         }
 
-        // If 'try', body and catches must all return true for it to be a returning stmt
-        if ($stmt.instanceOf("try")) {
-            let result = MutatorUtils.isReturningStmt($stmt.body);
+        // Apply to last statement of case
+        result = result & MutatorUtils.isReturningStmt(stmts[stmts.length - 1]);
+      }
 
-            for (var currentCatch of $stmt.catches) {
-                //println("CATCH AST:\n" + currentCatch.ast);
-                //println("CATCH BODY AST:\n" + currentCatch.body.ast);
-                result = result & MutatorUtils.isReturningStmt(currentCatch.body);
-            }
+      return result;
+    }
 
-            return result;
-        }
+    // Check last statement of body
+    if ($stmt.instanceOf("body")) {
+      //println("BODY")
+      println($stmt.ast);
 
-        // If 'switch', true if all cases return
-        if ($stmt.instanceOf('switch')) {
-            let result = true;
-            for (const switchCase of $stmt.cases) {
-                const stmts = switchCase.stmts;
+      const lastStmt = $stmt.lastStmt;
 
-                // If empty, assume fall-through to next case,
-                // leaving result unchanged
-                if (stmts.length === 0) {
-                    continue;
-                }
-
-                // Apply to last statement of case
-                result = result & MutatorUtils.isReturningStmt(stmts[stmts.length - 1]);
-            }
-
-            return result;
-        }
-
-        // Check last statement of body
-        if ($stmt.instanceOf('body')) {
-            //println("BODY")
-            println($stmt.ast);
-
-            const lastStmt = $stmt.lastStmt;
-
-            // No last statement, no return
-            if (lastStmt === undefined) {
-                return false;
-            }
-
-            return MutatorUtils.isReturningStmt(lastStmt);
-        }
-
-        //println("IS RETURN FALSE FOR: " + $stmt.joinPointType);
-
-
+      // No last statement, no return
+      if (lastStmt === undefined) {
         return false;
+      }
+
+      return MutatorUtils.isReturningStmt(lastStmt);
     }
 
+    //println("IS RETURN FALSE FOR: " + $stmt.joinPointType);
 
-    /**
-     * Generates a new mutant ID.
-     * 
-     * @param {*} fileName 
-     * @param {*} mutantCounter 
-     * @param {*} mutator 
-     * @returns 
-     */
-    static buildMutantId(fileName, mutantCounter, mutator) {
-                
-                // Since mutations are done independently by file, file name comes first 
-                return fileName.replace(".java", "") +
-                "_" +
-                // Counter is supposed to represent all possible mutation points, although certain approaches might not be able to support them
-                mutantCounter +
-                "_" +
-                mutator.getName();
-    }
+    return false;
+  }
 
+  /**
+   * Generates a new mutant ID.
+   *
+   * @param {*} fileName
+   * @param {*} mutantCounter
+   * @param {*} mutator
+   * @returns
+   */
+  static buildMutantId(fileName, mutantCounter, mutator) {
+    // Since mutations are done independently by file, file name comes first
+    return (
+      fileName.replace(".java", "") +
+      "_" +
+      // Counter is supposed to represent all possible mutation points, although certain approaches might not be able to support them
+      mutantCounter +
+      "_" +
+      mutator.getName()
+    );
+  }
 }
