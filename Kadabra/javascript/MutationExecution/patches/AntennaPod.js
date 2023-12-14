1db0c06f8e94d5528c0334537bfd3956a1eb331e
Auri Vincenzi <aurimrv@yahoo.com.br>
2023-12-14 18:48:13 +0000

    fix: improve patch code to minimize duplication

--- Kadabra/javascript/MutationExecution/patches/AntennaPod.js
+++ Kadabra/javascript/MutationExecution/patches/AntennaPod.js
@@ -6,72 +6,49 @@ laraImport("weaver.Query")
  * @param {file} file 
  */
 function patchFile(file) {
-    if(file.name === "DownloadsSection.java" || file.name === "SubscriptionsSection.java" || file.name === "EpisodesSurpriseSection.java" || file.name === "QueueSection.java" || file.name === "InboxSection.java" || file.name === "DownloadsSection.java") {
-        //println(Query.search("method", "onCreateView").first().ast)
-        patchViewBinding(file);
+    if(file.name === "SubscriptionsSection.java") {
+        // println(Query.search("method", "onEventMainThread").first().ast)
+        patchViewBinding(file, "onCreateView");
+    } else if(file.name === "DownloadsSection.java" 
+        || file.name === "QueueSection.java") {
+        patchViewBinding(file, "onCreateView");
+        patchViewBinding(file, "onEventMainThread");
+    } else if(file.name === "InboxSection.java" ) {
+        patchViewBinding(file, "onCreateView");
+        patchViewBinding(file, "loadItems");
+    } else if(file.name === "EpisodesSurpriseSection.java") {
+        patchViewBinding(file, "onCreateView");
+        patchViewBinding(file, "onEventMainThread");
+        // NOVO TESTE        
+        patchEpisodesSurpriseSection(file, "onCreateView");
     }
-    
-    // NOVO TESTE
-    if(file.name === "EpisodesSurpriseSection.java") {
-        patchEpisodesSurpriseSection(file);
-    }
-    
 }
 
-// NOVA FUNÇÃO
-function patchEpisodesSurpriseSection(file) {
-        for(const jp of Query.searchFrom(file, "method", "onCreateView").search("var", {code: code => code.startsWith("de.danoeh.antennapod.ui.home.HomeSection.viewBinding")})) {
-            jp.replaceWith("viewBinding")
-        }
+function patchEpisodesSurpriseSection(file, method) {
+    for(const jp of Query.searchFrom(file, "method", method).search("var", {code: code => code.startsWith("de.danoeh.antennapod.ui.home.HomeSection.viewBinding")})) {
+        jp.replaceWith("viewBinding")
+    }
 }
 
-function patchViewBinding(file) {
-    
+function patchViewBinding(file, method) {
     //method onCreateView
-    for(const fieldAccess of Query.searchFrom(file, "method", "onCreateView").search("fieldAccess", {code: code => code.endsWith(".viewBinding")})) {
-        const stmt = fieldAccess.ancestor("statement");
-        const stmtCode = stmt.code.trim();
-
-        if(stmtCode.startsWith("de.danoeh.antennapod.ui.home.HomeSection.viewBinding")) {
-            stmt.replaceWith(stmtCode.substring("de.danoeh.antennapod.ui.home.HomeSection.".length, stmtCode.length));                
-        } else {
-            const fieldAccessCode = fieldAccess.code;
-            fieldAccess.replaceWith(fieldAccessCode.substring("de.danoeh.antennapod.ui.home.HomeSection.".length, fieldAccessCode.length));
-        }
-        //println("Stmt: " + stmt.code);
-    }
-
-    //method onEventMainThread
-    for(const fieldAccess of Query.searchFrom(file, "method", "onEventMainThread").search("fieldAccess", {code: code => code.endsWith(".viewBinding")})) {
+    for(const fieldAccess of Query.searchFrom(file, "method", method).search("fieldAccess", {code: code => code.endsWith(".viewBinding")})) {
         //println("FOUND!\n" + fieldAccess.code)
         const stmt = fieldAccess.ancestor("statement");
-        const stmtCode = stmt.code.trim();
+        const stmtCode = stmt.code;
 
         if(stmtCode.startsWith("de.danoeh.antennapod.ui.home.HomeSection.viewBinding")) {
-            stmt.replaceWith(stmtCode.substring("de.danoeh.antennapod.ui.home.HomeSection.".length, stmtCode.length));                
+            println(method + " Original Stmt: " + stmtCode);
+            stmt.replaceWith(stmtCode.substring("de.danoeh.antennapod.ui.home.HomeSection.".length, stmtCode.length));
+            println(method + " Replaced Stmt: " + stmt.code);
         } else {
             const fieldAccessCode = fieldAccess.code;
+            println(method + " Original Field: " + fieldAccessCode);
             fieldAccess.replaceWith(fieldAccessCode.substring("de.danoeh.antennapod.ui.home.HomeSection.".length, fieldAccessCode.length));
+            println(method + " Replaced Field: " + fieldAccess.code);
         }
-        //println("Stmt: " + stmt.code);
-    }
-
-    //method loadItems
-    for(const fieldAccess of Query.searchFrom(file, "method", "loadItems").search("fieldAccess", {code: code => code.endsWith(".viewBinding")})) {
-        //println("FOUND!\n" + fieldAccess.code)
-        const stmt = fieldAccess.ancestor("statement");
-        const stmtCode = stmt.code.trim();
-
-        if(stmtCode.startsWith("de.danoeh.antennapod.ui.home.HomeSection.viewBinding")) {
-            stmt.replaceWith(stmtCode.substring("de.danoeh.antennapod.ui.home.HomeSection.".length, stmtCode.length));                
-        } else {
-            const fieldAccessCode = fieldAccess.code;
-            fieldAccess.replaceWith(fieldAccessCode.substring("de.danoeh.antennapod.ui.home.HomeSection.".length, fieldAccessCode.length));
-        }
-        //println("Stmt: " + stmt.code);
     }
 }
-
 
 globalThis.patchFile = patchFile;
 globalThis.AntennaPod = {"patchFile": patchFile}
